f6e8f1ca2fde0555b9b3ec9f582d2e78ff8b80ef
DESKTOP-GEKBC7S\enric <enric.robert@gmail.com>
2021-03-23 09:25:24 +0100

    aadads

---
+++ src/com/company/Taulell.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Taulell {
+
+    private int files;
+    private int columnes;
+    private float[][] taulell;
+
+    public Taulell() {
+        this.files = 0;
+        this.columnes = 0;
+        this.taulell = new float[files][columnes];
+    }
+
+    public Taulell(int f, int c) {
+        this.files = f;
+        this.columnes = c;
+        this.taulell = new float[files][columnes];
+    }
+public int getFiles{
+        getFiles
+    }
+}
